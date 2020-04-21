1f34d9b36af0dea8fa40c56aac6eb4811668647c
0x434D53 <christoph.seufert@gmail.com>
2020-04-21 14:24:32 +0200

    small fix after merge

--- pbmap/gen.go
+++ pbmap/gen.go
@@ -1,6 +1,6 @@
 package pbmap
 
-//go:generate sh -c "protoc -I. -I`go env GOPATH`/src --go_out=. *.proto"
+//go:generate sh -c "protoc -I. -I`go env GOPATH`/src --go_out=paths=source_relative:. *.proto"
 
 // NOTE fix invalid import pathes in generated file
 //go:generate sh -c "sed -i'.bak' -e'/^import google_protobuf/s,.*,import google_protobuf \"github.com/golang/protobuf/protoc-gen-go/descriptor\",' pbmap.pb.go"
